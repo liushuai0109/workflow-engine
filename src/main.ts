adf81416d8392995fa52776ee1f9ba726d0803b8
simonsliu <simonsliu@tencent.com>
2025-10-24 10:57:56 +0800

    Remove unused styles.css file and clean up main.ts by removing the import statement for styles.css, improving project organization and reducing unnecessary dependencies.

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import 'bpmn-js/dist/assets/diagram-js.css';
 import 'bpmn-js/dist/assets/bpmn-js.css';
 import 'bpmn-js/dist/assets/bpmn-font/css/bpmn-embedded.css';
 import '@bpmn-io/properties-panel/assets/properties-panel.css';
-import './styles.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
