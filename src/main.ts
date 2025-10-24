486286cbb7b08f2f426698b1dc53978f95554e6d
simonsliu <simonsliu@tencent.com>
2025-10-24 15:58:55 +0800

    Add styles.css for custom styling in BPMN Explorer and update main.ts to include the new stylesheet, enhancing the visual presentation of the application.

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import 'bpmn-js/dist/assets/diagram-js.css';
 import 'bpmn-js/dist/assets/bpmn-js.css';
 import 'bpmn-js/dist/assets/bpmn-font/css/bpmn-embedded.css';
 import '@bpmn-io/properties-panel/assets/properties-panel.css';
+import './style.css';
 
 import { createApp } from 'vue'
 import App from './App.vue'
